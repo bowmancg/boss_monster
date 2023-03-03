3b83c79ec45647d9f32e142e64090296ff224398
Chandler Bowman <doctorgrant99@gmail.com>
2023-03-02 19:59:07 -0700

    reworked boss attack functions

--- app.js
+++ app.js
@@ -23,6 +23,11 @@ const boss = {
     level: 1
 }
 
+const bossInterval = setInterval(bossAttacksSlate, 5000)
+
+const bossInterval2 = setInterval(bossAttacksFlint, 5000)
+
+
 function slateAttacksBoss(name) {
     let hero = heroes.find(h => h.name == 'Slate Slabrock')
     let slateDamage = hero.damage
@@ -30,7 +35,7 @@ function slateAttacksBoss(name) {
         boss.maxHealth -= slateDamage
         console.log(boss);
     }
-    
+
     drawBossHealth()
 }
 
@@ -44,35 +49,51 @@ function flintAttacksBoss(name) {
     drawBossHealth()
 }
 
-// function bossAttacksHeroes(heroes) {
-//     let damage = 0
-//     heroes.filter(hero => hero.name = hero) {
+function bossAttacksSlate() {
+    let hero = heroes.find(h => h.name === 'Slate Slabrock')
+    if (hero.health <= 0) {
+        clearInterval(bossInterval)
+    } else {
+        hero.health -= 5
+    }
+    console.log('slate');
+    drawHeroesHealth()
+}
 
-//     }
-    
-// }
-
-function letsKillSlater() {
-    heroes.find(hero => hero.name === 'Slater Slabrock')
-    
+function bossAttacksFlint() {
+    let hero = heroes.find(h => h.name === 'Flint Ironstag')
+    if (hero.health <= 0) {
+        clearInterval(bossInterval2)
+    } else {
+        hero.health -= 5
+    }
+    console.log('flint');
+    drawHeroesHealth()
 }
 
 // function updateSlateStatus(heroes) {
 //     heroes.forEach(hero => hero.name === 'Slate Slabrock')
 //     for (hero.maxHealth = 100; hero.maxHealth <= 100; hero.maxHealth--) {
 //         if(hero.maxHealth <= 0) {
-//             hero.maxHealth === 0
+//             hero.maxHealth == 0
 //         }
 //     }
 // }
 
+
+
 function drawHeroesHealth() {
     let template = ''
     heroes.forEach(hero => {
-        template += `${heroes.name}`
+        switch (hero.name) {
+            case 'Slate Slabrock':
+                document.getElementById('hero-1').innerHTML = hero.health
+                break
+            case 'Flint Ironstag':
+                document.getElementById('hero-2').innerHTML = hero.health
+                break
+        }
     });
-    document.getElementById('hero-1').innerHTML = template
-    document.getElementById('hero-2').innerHTML = template
 }
 
 // drawHeroesHealth() {
@@ -84,4 +105,3 @@ function drawBossHealth() {
     smaug.innerText = boss.maxHealth
 }
 
-setInterval(bossAttacksHeroes, 5000)
